74d3caf8220e74b7a310cf51870a215eb238bc39
Vampyr7878 <vampyr7878@gmail.com>
2019-08-02 03:25:15 +0200

    Add files

---
+++ OBLExporter/OBLExporter.cs
@@ -0,0 +1,86 @@
+﻿using GXLib;
+using LXFLib;
+using System.IO;
+using System.Numerics;
+
+namespace OBLExporter
+{
+    /// <summary>
+    /// Exporter class.
+    /// </summary>
+    public class OBLExporter
+    {
+        /// <summary>
+        /// Export the model.
+        /// </summary>
+        /// <param name="model">Model to be exported.</param>
+        /// <param name="file">Output file.</param>
+        public static void Export(LXF model, string file)
+        {
+            // The output file is a slightly modified obj file to allow for lego materials
+            // and model structure so you know which parts are attached to each other.
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                int a, b, c;
+                // File name.
+                writer.WriteLine("# " + Path.GetFileName(file));
+                foreach (Brick brick in model.Bricks)
+                {
+                    int i = 0;
+                    // Make a group for each part.
+                    foreach (GX part in brick.Models)
+                    {
+                        writer.WriteLine();
+                        // Name of the group is the part number plus a subpart number.
+                        writer.WriteLine("g " + brick.Name + "x" + i);
+                        writer.WriteLine();
+                        // Make bone for the piece, used to attach them.
+                        writer.WriteLine("b " + brick.Bones[0]);
+                        writer.WriteLine();
+                        if (i == 0)
+                        {
+                            // If it's the main subpart then include a parent bone.
+                            writer.WriteLine("p " + brick.Parent);
+                            writer.WriteLine();
+                            // If it's the main subpart then include transformation matrix.
+                            writer.Write("t");
+                            for (int j = 0; j < 16; j++)
+                            {
+                                writer.Write(" " + brick.Transformation[j]);
+                            }
+                            writer.WriteLine();
+                            writer.WriteLine();
+                        }
+                        // Index of a Lego Material.
+                        writer.WriteLine("m " + part.Material);
+                        writer.WriteLine();
+                        // Number of vertices and their coordinates.
+                        writer.WriteLine(part.Vertices.Length);
+                        foreach (Vector3 v in part.Vertices)
+                        {
+                            writer.WriteLine("v " + v.X + " " + v.Y + " " + v.Z);
+                        }
+                        // Number of normals and their coordinates.
+                        writer.WriteLine();
+                        writer.WriteLine(part.Normals.Length);
+                        foreach (Vector3 v in part.Normals)
+                        {
+                            writer.WriteLine("vn " + v.X + " " + v.Y + " " + v.Z);
+                        }
+                        // Number of vertex indices and their order for each face.
+                        writer.WriteLine();
+                        writer.WriteLine(part.Indices.Length);
+                        for (int j = 0; j < part.Indices.Length; j++)
+                        {
+                            a = part.Indices[j++];
+                            b = part.Indices[j++];
+                            c = part.Indices[j];
+                            writer.WriteLine("f " + a + " " + b + " " + c);
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
